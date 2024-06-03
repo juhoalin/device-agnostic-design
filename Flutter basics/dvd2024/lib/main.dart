0a6aa479bf5505540b0e3b914ee6f3972dc9b14f
juhoalin <63160665+juhoalin@users.noreply.github.com>
2024-06-03 09:16:31 +0300

    try editing texts

--- Flutter basics/dvd2024/lib/main.dart
+++ Flutter basics/dvd2024/lib/main.dart
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 64, 183, 58)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 64, 183, 58)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Vittu tää toimii'),
